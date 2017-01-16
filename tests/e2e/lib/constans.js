0528c207976946870a46e15c436cd6368bc6d912
Bartlomiej Szewczyk <bartlomiej.szewczyk@onebi.eu>
2017-01-16 21:41:36 +0100

    Added constans.js and page_helpers.js Added and updated tests create_test.js

---
+++ tests/e2e/lib/constans.js
@@ -0,0 +1,13 @@
+var pageConstans = function() {
+    WEBPAGE_URL = {
+        COMPUTER_DATABASE: "http://computer-database.herokuapp.com/computers"
+    };
+
+    BY = {
+        ID: "id",
+        XPATH: 'xpath',
+        CSS: 'css'
+    }
+};
+
+module.exports = new pageConstans();
