cc4b4aa85f7f90779555a0e1cd74d8dd3d43fa4e
damian.kwasny <damian.kwasny@soflab.pl>
2017-01-16 14:27:41 +0100

    my first commit

--- tests/e2e/tests/spec.js
+++ tests/e2e/tests/spec.js
@@ -1,7 +1,8 @@
 describe('Protractor Demo App', function() {
   it('should have a title', function() {
+    browser.ignoreSynchronization = true;
     browser.get('http://computer-database.herokuapp.com/computers');
 
-    expect(browser.getTitle()).toEqual('Super Calculator');
+    expect(browser.getTitle()).toEqual('Computer database');
   });
 });
