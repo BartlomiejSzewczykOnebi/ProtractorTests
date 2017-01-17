ffbda63f4323807877d138dcb0172413d8c77703
Filip Szpila <filip.szpila@soflab.pl>
2017-01-17 14:48:41 +0100

    modified update_test.js

--- tests/e2e/tests/update_test.js
+++ tests/e2e/tests/update_test.js
@@ -1,63 +1,51 @@
 'use strict';
 require("../helpers/page_helpers.js");
-require("../lib/constans.js");
-var EditDeleteComputerPage = require("../pages/add_computer_page.js");
+require("../lib/constants.js");
+var EditDeleteComputerPage = require("../pages/edit_delete_computer_page.js");
+var ComputersDatabaseList = require("../pages/computers_database_list_page.js");
 
 describe("Computer database tests - update computer", () => {
 
     browser.ignoreSynchronization = true;
-    var addComputerPage;
+    var editDeleteComputerPage = new EditDeleteComputerPage();
+    var computersDatabaseList = new ComputersDatabaseList();
 
-    beforeEach(() =>{
-        addComputerPage = new AddComputerPage();
-    });
-
-    let computerName = "IBM 7030";
-        introducedDate = "1961-01-01";
-        discontinuedDate = "";
+    let computerName = "Blue Dragon",
+        introducedDate = "1961-01-01",
+        discontinuedDate = "",
         company = "IBM";
 
     it("Open webpage", () => {
         browser.get("http://computer-database.herokuapp.com/computers");
-        expect(browser.getTitle()).toEqual("Computers database");
+        expect(browser.getTitle()).toEqual("Computers database", "Web page title doesn't equals text: Computers database.");
     });
 
-    it("Insert text into search box", () => {
+    it("Insert text into search box and filter results", () => {
 
-        element(by.id("searchbox")).sendKeys(computerName);
-    });
-
-    it("Click filter by results", () => {
-
-        element(by.id("searchsubmit")).click();
-        click("searchsubmit");
+        computersDatabaseList.filterList(computerName);
     });
 
     it("Search for element in list", () => {
-        element.all(by.xpath("//*[@id='main']/table/tbody/tr/td[1]/a")).map((element)=>{
-            element.getText().then((text) => {
-                if(text == computerName) {
-                    element.click();
-                }
+        var els = element.all(by.xpath("//*[@id='main']/table/tbody/tr/td[1]/a"));
+           els.filter(function(elem) {
+            return elem.getText().then(function(text) {
+                return text === computerName;
             });
-        });
+        }).first().click();
     });
 
     it("Header text is Edit computer", () => {
 
-        let header = browser.findElement(By.xpath("//*[@id='main']/h1"));
-        expect(header.getText()).toEqual("Edit computer");
+        expect(editDeleteComputerPage.headerText.getText()).toEqual("Edit computer");
     });
 
     it("Provide input elements", () => {
-        element(by.id("introduced")).clear();
-        element(by.id("introduced")).sendKeys(introducedDate);
-        element(by.id("discontinued")).clear();
-        element(by.id("discontinued")).sendKeys(discontinuedDate);
+        editDeleteComputerPage.changeIntroducedDate(introducedDate);
+        editDeleteComputerPage.changeDiscontinuedDate(discontinuedDate);
     });
 
     it("Click Save this computer", () => {
 
-        element(by.xpath("//div[@class='actions']/input")).click();
+        editDeleteComputerPage.clickSave();
     });
 });
