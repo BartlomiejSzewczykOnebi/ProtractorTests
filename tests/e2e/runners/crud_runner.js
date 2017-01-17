b6ff817f013d7b26902f436e54385a881a1fe88f
damian.kwasny <damian.kwasny@soflab.pl>
2017-01-17 11:04:30 +0100

    CompuersDatabaseList page object

--- tests/e2e/runners/crud_runner.js
+++ tests/e2e/runners/crud_runner.js
@@ -1,11 +1,11 @@
 exports.config = {
-  framework: 'jasmine',
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['../tests/create_test.js'],
-  capabilities: {
-    'browserName': 'chrome',
-    'chromeOptions': {
-        'args': ['--start-maximized']
+    framework: 'jasmine',
+    seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['../tests/create_test.js'],
+    capabilities: {
+        'browserName': 'chrome',
+        'chromeOptions': {
+            'args': ['--start-maximized']
+        }
     }
-}
 };
--- tests/e2e/tests/read_test.js
+++ tests/e2e/tests/read_test.js
@@ -1,34 +1,23 @@
-require("../helpers/page_helpers.js");
-require("../lib/constans.js");
+var ComputersDatabaseList = require("../pages/computers_database_list.js");
 
 describe('Computer database tests - check computer name', function () {
     browser.ignoreSynchronization = true;
+    computersDatabaseListPage = new ComputersDatabaseList();
+
     var computerName = "Black Moon";
 
     it('Open webpage', () => {
-        browser.get("http://computer-database.herokuapp.com/computers");
         expect(browser.getTitle()).toEqual("Computers database");
     });
 
     it('Filter list', () => {
-        typeValue(computerName, "searchbox");
-        click("searchsubmit");
+        computersDatabaseListPage.filterList(computerName);
+        expect(computersDatabaseListPage.computersList.count()).toBeGreaterThan(0);
     });
 
     it('Filtered list should contains expexted value', () => {
-        var listContainsValue = false;
-
-        element.all(by.xpath("//table[contains(@class,'computers')]/tbody/tr/td/a")).each((element) => {
-            element.getText().then((text)=> {
-                if (text == computerName)
-                    listContainsValue = true;
-            });
-
-            return listContainsValue;
-        }).then(()=> {
-            expect(listContainsValue).toBe(true, "List of computers not contain expected value: '" + computerName + "'.");
-            element.all(by.xpath("//table[contains(@class,'computers')]/tbody/tr/td/a")).first().click();
-        });
+        expect(computersDatabaseListPage.computersList.getText()).toContain(computerName);
+        computersDatabaseListPage.clickComputerNameAt(0);
     });
 
     it("Header text is Edit computer", () => {
