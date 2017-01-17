7078d3e6d2a9727f7977870d9ffc55a49991e6bd
Bartlomiej Szewczyk <bartlomiej.szewczyk@onebi.eu>
2017-01-17 15:05:40 +0100

    Added update computer validation test

--- tests/e2e/tests/update_test.js
+++ tests/e2e/tests/update_test.js
@@ -7,8 +7,8 @@ var ComputersDatabaseList = require("../pages/computers_database_list_page.js");
 describe("Computer database tests - update computer", () => {
 
     browser.ignoreSynchronization = true;
-    var editDeleteComputerPage = new EditDeleteComputerPage();
     var computersDatabaseList = new ComputersDatabaseList();
+    var editDeleteComputerPage = new EditDeleteComputerPage();
 
     let computerName = "Blue Dragon",
         introducedDate = "1961-01-01",
@@ -16,12 +16,10 @@ describe("Computer database tests - update computer", () => {
         company = "IBM";
 
     it("Open webpage", () => {
-        browser.get("http://computer-database.herokuapp.com/computers");
         expect(browser.getTitle()).toEqual("Computers database", "Web page title doesn't equals text: Computers database.");
     });
 
     it("Insert text into search box and filter results", () => {
-
         computersDatabaseList.filterList(computerName);
     });
 
@@ -35,7 +33,6 @@ describe("Computer database tests - update computer", () => {
     });
 
     it("Header text is Edit computer", () => {
-
         expect(editDeleteComputerPage.headerText.getText()).toEqual("Edit computer");
     });
 
@@ -45,7 +42,57 @@ describe("Computer database tests - update computer", () => {
     });
 
     it("Click Save this computer", () => {
-
         editDeleteComputerPage.clickSave();
+    });
+});
+
+describe('Computer database tests - update computer validation', () => {
+
+    browser.ignoreSynchronization = true;
+    var computersDatabaseListPage = new ComputersDatabaseList();
+    var editDeleteComputerPage = new EditDeleteComputerPage();
+
+    var computerName = "Blue Dragon",
+        introducedDate = "20160810",
+        discontinuedDate = "20170105";
+
+    it('Open webpage', () => {
+        expect(browser.getTitle()).toEqual("Computers database", "Web page title doesn't equals text: Computers database.");
+    });
+
+    it('Filter list is not empty', () => {
+        computersDatabaseListPage.filterList(computerName);
+        expect(computersDatabaseListPage.computersList.count()).toBeGreaterThan(0);
+    });
+
+    it('Filtered list contains expected value', () => {
+        expect(computersDatabaseListPage.computersList.getText()).toContain(computerName);
+        computersDatabaseListPage.clickComputerNameAt(0);
+    });
+
+    it("Header text is Edit computer", () => {
+        expect(editDeleteComputerPage.headerText.getText()).toEqual("Edit computer");
+    });
+
+    it('Check validation texts', () => {
+        expect(editDeleteComputerPage.computerNameValidationText).toEqual("Required", "Validation text doesn't equals text: Required.");
+        expect(editDeleteComputerPage.introducedDateValidationText).toEqual("Date ('yyyy-MM-dd')", "Validation text doesn't equals text: Date ('yyyy-MM-dd').");
+        expect(editDeleteComputerPage.discontinuedDateValidationText).toEqual("Date ('yyyy-MM-dd')", "Validation text doesn't equals text: Date ('yyyy-MM-dd').");
+    });
+
+    it('Type text into input elements', () => {
+        editDeleteComputerPage.clearComputerName();
+        editDeleteComputerPage.changeIntroducedDate(introducedDate);
+        editDeleteComputerPage.changeDiscontinuedDate(discontinuedDate);
+    });
+
+    it("Click Save this computer", () => {
+        editDeleteComputerPage.clickSave();
+    });
+
+    it('Check if validation appear', () => {
+        expect(editDeleteComputerPage.computerNameValidationClass).toContain("error", "Field doesn't show validation");
+        expect(editDeleteComputerPage.introducedDateValidationClass).toContain("error", "Field doesn't show validation");
+        expect(editDeleteComputerPage.discontinuedDateValidationClass).toContain("error", "Field doesn't show validation");
     });
 });
