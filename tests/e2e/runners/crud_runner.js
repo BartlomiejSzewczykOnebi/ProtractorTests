5948d9a5c8c8ad74972071280bfb3e1e69e7feb2
damian.kwasny <damian.kwasny@soflab.pl>
2017-01-18 10:51:07 +0100

    reports module

--- tests/e2e/runners/crud_runner.js
+++ tests/e2e/runners/crud_runner.js
@@ -2,10 +2,24 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['../tests/update_test.js'],
+    suites: {
+        createTest: ['../tests/create_test.js'],
+        readTest: ['../tests/read_test.js'],
+        updateTest: ['../tests/update_test.js'],
+        deleteTest: ['../tests/delete_test.js']
+    },
     capabilities: {
         'browserName': 'chrome',
         'chromeOptions': {
             'args': ['--start-maximized']
         }
+    },
+    onPrepare: function() {
+        var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+        jasmine.getEnv().addReporter(
+            new Jasmine2HtmlReporter({
+                savePath: 'target/screenshots'
+            })
+        );
     }
 };
